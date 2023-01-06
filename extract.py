3a24e0c88bf558be7a88de6eec9694d6ca95d911
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-01-06 15:59:56 +0000

    extract

--- extract.py
+++ extract.py
@@ -1,38 +1,73 @@
 import tweepy
-import pandas as pd 
+
+def extract_tweet(user = 'elonmusk')  : 
+
+    """ allows you to retrieve all the data related to the user account and to store the data as a dictionary """
+
+    access_key = "PiZ9HwCmjN9NUkdxGdabbHVSV" 
+    access_secret = "TkxotsQVGI6zErHNDeKkpKBttPtKCICxfQTfKxWHkR8b1yCYiC" 
+    consumer_key = "1572886741-D4cmA6kTxR3ctyHC54CP8S5TddMe8WNcPW59xR4"
+    consumer_secret = "lf03CiqS2ECm6vkgDPlmlSVHQ3BxoIU9zqgaoFbxg3wyX"
+
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(access_key, access_secret)   
+    auth.set_access_token(consumer_key, consumer_secret) 
+
+    # Creating an API object 
+    api = tweepy.API(auth)
+
+    # store info 
+    infos = {
+        user: {
+            "TWEET_INFO": {
+                "text": [] ,
+                "favorite_count":[] ,
+                "retweet_count": [],
+                "created_at":[]
+            },
+            "USER_INFO": {
+                "followers_count":'' ,
+                "following_count": '',
+                "created_at":'' ,
+                "description":'' , 
+                "location":''
+            },
+            "USER_ACTIVITY": {
+                "favoris_count":'' ,
+                "retweet_count":'' ,
+            }
+        }
+    }
+
+    # get user tweets 
+    tweets = api.user_timeline(screen_name='@'+str(user), 
+                            count=100,
+                            include_rts = False,
+                            tweet_mode = 'extended') 
+
+    # retrieves tweets
+    for tweet in tweets : 
+        infos[user]["TWEET_INFO"]['text'].append(tweet._json["full_text"])
+        infos[user]["TWEET_INFO"]['favorite_count'].append(tweet.favorite_count)
+        infos[user]["TWEET_INFO"]['retweet_count'].append(tweet.retweet_count)
+        infos[user]["TWEET_INFO"]['created_at'].append(tweet.created_at)
+  
+    # Retrieving account information
+    user = api.get_user( screen_name = user)
+    
+    infos[user]["USER_INFO"]['followers_count'].append( user.followers_count)
+    infos[user]["USER_INFO"]['following_count'].append( user.friends_count)
+    infos[user]["USER_INFO"]['created_at'].append(user.created_at)
+    infos[user]["USER_INFO"]['description'].append(user.description)
+    infos[user]["USER_INFO"]['location'].append(user.location)
 
 
-access_key = "PiZ9HwCmjN9NUkdxGdabbHVSV" 
-access_secret = "TkxotsQVGI6zErHNDeKkpKBttPtKCICxfQTfKxWHkR8b1yCYiC" 
-consumer_key = "1572886741-D4cmA6kTxR3ctyHC54CP8S5TddMe8WNcPW59xR4"
-consumer_secret = "lf03CiqS2ECm6vkgDPlmlSVHQ3BxoIU9zqgaoFbxg3wyX"
+    # Retrieving activity account information
+    user = api.get_user( screen_name = user)
+    
+    infos[user]["USER_ACTIVITY"]['favoris_count'].append( user.followers_count)
+    infos[user]["USER_ACTIVITY"]['retweet_count'].append( user.friends_count)
 
-
-# Twitter authentication
-auth = tweepy.OAuthHandler(access_key, access_secret)   
-auth.set_access_token(consumer_key, consumer_secret) 
-
-
-## Creating an API object 
-api = tweepy.API(auth)
-
-# get  user tweets 
-tweets = api.user_timeline(screen_name='@elonmusk', 
-                        count=100,
-                        include_rts = False,
-                        # Necessary to keep full_text 
-                        # otherwise only the first 140 words are extracted
-                        tweet_mode = 'extended'
-                        )
-
-for tweet in tweets : 
-    text = tweet._json["full_text"]
-
-    refined_tweet = {"user": tweet.user.screen_name,
-                        'text' : text,
-                        'favorite_count' : tweet.favorite_count,
-                        'retweet_count' : tweet.retweet_count,
-                        'created_at' : tweet.created_at}
-    print(refined_tweet)
+    return infos     
 
 
---
+++ test.py
@@ -0,0 +1,24 @@
+infos = {
+    'ok': {
+        "TWEET_INFO": {
+            "text": ,
+            "favorite_count": ,
+            "retweet_count": ,
+            "created_at":
+        },
+        "USER_INFO": {
+            "friends_count": ,
+            "created_at": ,
+            "retweet_count": ,
+            "created_at": ,
+            "description":
+        },
+        "USER_ACTIVITY": {
+            "favoris_count": ,
+            "retweet_count": ,
+            "location":
+        }
+    }
+}
+
+print('ok')
